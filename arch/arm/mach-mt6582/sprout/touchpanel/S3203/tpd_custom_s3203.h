8c1a8f30c6e28f577865259b1e223f900ca0bced
Anmin Hsu <anmin.hsu@mediatek.com>
2014-05-22 19:45:42 +0800

    input: touchscreen: mediatek: s3203: Update Sprout V2 Phone Touchpanel driver
    Change-Id: I04f2beeb2cccd1565101a554923c5dfdd78b5c45
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

---
+++ arch/arm/mach-mt6582/sprout/touchpanel/S3203/tpd_custom_s3203.h
@@ -0,0 +1,36 @@
+#ifndef TOUCHPANEL_H__
+#define TOUCHPANEL_H__
+
+/* Pre-defined definition */
+
+/* Register */
+#define FD_ADDR_MAX        0xE9
+#define FD_ADDR_MIN        0xDD
+#define FD_BYTE_COUNT     6
+
+#define CUSTOM_MAX_WIDTH (600)
+#define CUSTOM_MAX_HEIGHT (800)
+
+//#define TPD_UPDATE_FIRMWARE
+
+#define TPD_HAVE_BUTTON
+#define TPD_BUTTON_HEIGH        (100)
+#define TPD_KEY_COUNT           3
+#define TPD_KEYS                { KEY_BACK, KEY_HOMEPAGE, KEY_MENU}
+#define TPD_KEYS_DIM        {{100,854,120,TPD_BUTTON_HEIGH},\
+                            {300,854,120,TPD_BUTTON_HEIGH},\
+                            {500,854,120,TPD_BUTTON_HEIGH}}
+
+#define TPD_POWER_SOURCE_CUSTOM         MT6323_POWER_LDO_VGP1//MT65XX_POWER_LDO_VGP1
+
+//#define TPD_HAVE_CALIBRATION
+//#define TPD_CALIBRATION_MATRIX  {2680,0,0,0,2760,0,0,0};
+//#define TPD_WARP_START
+//#define TPD_WARP_END
+
+//#define TPD_RESET_ISSUE_WORKAROUND
+//#define TPD_MAX_RESET_COUNT 3
+//#define TPD_WARP_Y(y) ( TPD_Y_RES - 1 - y )
+//#define TPD_WARP_X(x) ( x )
+
+#endif /* TOUCHPANEL_H__ */
