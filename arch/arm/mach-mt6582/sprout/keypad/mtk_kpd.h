301f97293eb0b5e72e9154da1c613d3e2ab97599
Anmin Hsu <anmin.hsu@mediatek.com>
2014-05-22 17:45:49 +0800

    ARM: mt6582: sprout: modify kpd hardware key code and linux key code mapping
    Change-Id: I6715d90ef14e479f967d87bd58e1b1fb8adb6486
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

--- arch/arm/mach-mt6582/sprout/keypad/mtk_kpd.h
+++ arch/arm/mach-mt6582/sprout/keypad/mtk_kpd.h
@@ -79,7 +79,7 @@ void kpd_pmic_rstkey_handler(unsigned long pressed);
 #define TWOKEY_REBOOT_NORMAL_MODE
 //#define ONEKEY_REBOOT_OTHER_MODE
 #define TWOKEY_REBOOT_OTHER_MODE
-#define KPD_PMIC_RSTKEY_MAP KEY_VOLUMEUP
+#define KPD_PMIC_RSTKEY_MAP KEY_VOLUMEDOWN
 #define KPD_PMIC_LPRST_TD 1 /* timeout period. 0: 7sec; 1: 11sec; 2: 14sec; 3: 5sec */
 
 #endif
