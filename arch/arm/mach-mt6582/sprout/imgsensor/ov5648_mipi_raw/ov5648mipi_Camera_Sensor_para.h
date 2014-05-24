65b000dac7a02f62faf485880a202f9e982b8eba
Alvin Huang <alvincl.huang@mediatek.com>
2014-05-24 10:58:23 +0800

    ARM: mt6582: sprout: Update imgsensor for sprout
    Change-Id: I6a678b5a714c62b23d05f9859e2e982ba2b0b6f4
    Signed-off-by: Alvin Huang <alvincl.huang@mediatek.com>

--- arch/arm/mach-mt6582/sprout/imgsensor/ov5648_mipi_raw/ov5648mipi_Camera_Sensor_para.h
+++ arch/arm/mach-mt6582/sprout/imgsensor/ov5648_mipi_raw/ov5648mipi_Camera_Sensor_para.h
@@ -1,25 +1,24 @@
 /*
 * Copyright (C) 2011-2014 MediaTek Inc.
-* 
-* This program is free software: you can redistribute it and/or modify it under the terms of the 
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the
 * GNU General Public License version 2 as published by the Free Software Foundation.
-* 
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * See the GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License along with this program.
 * If not, see <http://www.gnu.org/licenses/>.
 */
-
 /*******************************************************************************************/
 
 
 /*******************************************************************************************/
 
 /* SENSOR FULL SIZE */
-#ifndef __CAMERA_SENSOR_PARA_H
-#define __CAMERA_SENSOR_PARA_H
+#ifndef _OV5648MIPI_CAMERA_SENSOR_PARA_H
+#define _OV5648MIPI_CAMERA_SENSOR_PARA_H
 
 #define CAMERA_SENSOR_REG_DEFAULT_VALUE  \
     /* ARRAY: SENSOR.reg[11] */\
