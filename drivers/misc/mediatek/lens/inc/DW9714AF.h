e9ab8d79d3ed75d363c92f4a5e534c9e1544c825
Anmin Hsu <anmin.hsu@mediatek.com>
2014-05-28 14:38:45 +0800

    Sprout V1/V2 code sync to google
    Change-Id: I61fa7f5e55c78d63d74d3aaadc25949a20728dc9
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

---
+++ drivers/misc/mediatek/lens/inc/DW9714AF.h
@@ -0,0 +1,95 @@
+/*
+* Copyright (C) 2011-2014 MediaTek Inc.
+* 
+* This program is free software: you can redistribute it and/or modify it under the terms of the 
+* GNU General Public License version 2 as published by the Free Software Foundation.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _DW9714AF_H
+#define _DW9714AF_H
+
+#include <linux/ioctl.h>
+//#include "kd_imgsensor.h"
+
+#define DW9714AF_MAGIC 'A'
+//IOCTRL(inode * ,file * ,cmd ,arg )
+
+
+//Structures
+typedef struct {
+//current position
+unsigned long u4CurrentPosition;
+//macro position
+unsigned long u4MacroPosition;
+//Infiniti position
+unsigned long u4InfPosition;
+//Motor Status
+bool          bIsMotorMoving;
+//Motor Open?
+bool          bIsMotorOpen;
+//Support SR?
+bool          bIsSupportSR;
+} stDW9714AF_MotorInfo;
+
+
+//Structures
+//#define LensdrvCM3
+#ifdef LensdrvCM3
+typedef struct {
+    //APERTURE , won't be supported on most devices.
+    float Aperture;
+    //FILTER_DENSITY, won't be supported on most devices.
+    float FilterDensity;
+    //FOCAL_LENGTH, lens optical zoom setting. won't be supported on most devices.
+    float FocalLength;
+    //FOCAL_DISTANCE, current focus distance, lens to objects.
+    float FocalDistance;
+    //OPTICAL_STABILIZATION_MODE
+    u16 u4OIS_Mode;
+    //FACING
+    u16 Facing;
+    //Optical axis angle,  optical axis is perpendicular to LCM, usually is  {0,0}.
+    float OpticalAxisAng[2];
+    //Optical axis position (mm),  usually is  {0,0,0}.
+    float Position[3];
+    //Focus range, DOF,
+    float FocusRange;
+    //State
+    u16 State;
+    //INFO
+    float InfoAvalibleMinFocusDistance;
+    float InfoAvalibleApertures;
+    float InfoAvalibleFilterDensity;
+    u16 InfoAvalibleOptStabilization;
+    float InfoAvalibleFocalLength;
+    float InfoAvalibleHypeDistance;
+}stDW9714AF_MotorMETAInfo;
+#endif
+
+//Control commnad
+//S means "set through a ptr"
+//T means "tell by a arg value"
+//G means "get by a ptr"
+//Q means "get by return a value"
+//X means "switch G and S atomically"
+//H means "switch T and Q atomically"
+#define DW9714AFIOC_G_MOTORINFO _IOR(DW9714AF_MAGIC,0,stDW9714AF_MotorInfo)
+
+#define DW9714AFIOC_T_MOVETO _IOW(DW9714AF_MAGIC,1,unsigned long)
+
+#define DW9714AFIOC_T_SETINFPOS _IOW(DW9714AF_MAGIC,2,unsigned long)
+
+#define DW9714AFIOC_T_SETMACROPOS _IOW(DW9714AF_MAGIC,3,unsigned long)
+#ifdef LensdrvCM3
+#define DW9714AFIOC_G_MOTORMETAINFO _IOR(DW9714AF_MAGIC,4,stDW9714AF_MotorMETAInfo)
+#endif
+
+#else
+#endif
