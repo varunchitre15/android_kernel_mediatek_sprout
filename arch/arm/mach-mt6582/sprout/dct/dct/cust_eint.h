dd1d3110c8d34a271e2dae4e4153502db801ba64
Anmin Hsu <anmin.hsu@mediatek.com>
2014-05-28 14:56:56 +0800

    ARM: mt6582: sprout: Change EINT settings for headset detection and G-Sensor significant motion
    Change-Id: Ifc362568083df6d667f6ef48ea77c859725de815
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

--- arch/arm/mach-mt6582/sprout/dct/dct/cust_eint.h
+++ arch/arm/mach-mt6582/sprout/dct/dct/cust_eint.h
@@ -17,38 +17,53 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define CUST_EINTF_TRIGGER_RISING     			1    //High Polarity and Edge Sensitive
-#define CUST_EINTF_TRIGGER_FALLING    			2    //Low Polarity and Edge Sensitive
-#define CUST_EINTF_TRIGGER_HIGH      				4    //High Polarity and Level Sensitive
-#define CUST_EINTF_TRIGGER_LOW       				8    //Low Polarity and Level Sensitive
+#define CUST_EINTF_TRIGGER_RISING                 1    //High Polarity and Edge Sensitive
+#define CUST_EINTF_TRIGGER_FALLING                2    //Low Polarity and Edge Sensitive
+#define CUST_EINTF_TRIGGER_HIGH                      4    //High Polarity and Level Sensitive
+#define CUST_EINTF_TRIGGER_LOW                       8    //Low Polarity and Level Sensitive
 #define CUST_EINT_DEBOUNCE_DISABLE          0
 #define CUST_EINT_DEBOUNCE_ENABLE           1
 //////////////////////////////////////////////////////////////////////////////
 
 
 #define CUST_EINT_ALS_NUM              1
-#define CUST_EINT_ALS_DEBOUNCE_CN      16
-#define CUST_EINT_ALS_TYPE							CUST_EINTF_TRIGGER_LOW
-#define CUST_EINT_ALS_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_ENABLE
+#define CUST_EINT_ALS_DEBOUNCE_CN      0
+#define CUST_EINT_ALS_TYPE                            CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_ALS_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
 
 #define CUST_EINT_TOUCH_PANEL_NUM              2
 #define CUST_EINT_TOUCH_PANEL_DEBOUNCE_CN      0
-#define CUST_EINT_TOUCH_PANEL_TYPE							CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_TOUCH_PANEL_TYPE                            CUST_EINTF_TRIGGER_FALLING
 #define CUST_EINT_TOUCH_PANEL_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
 
-#define CUST_EINT_ACCDET_NUM              4
-#define CUST_EINT_ACCDET_DEBOUNCE_CN      256
-#define CUST_EINT_ACCDET_TYPE							CUST_EINTF_TRIGGER_LOW
-#define CUST_EINT_ACCDET_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_ENABLE
+#define CUST_EINT_MSE_NUM              3
+#define CUST_EINT_MSE_DEBOUNCE_CN      0
+#define CUST_EINT_MSE_TYPE                            CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_MSE_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
 
-#define CUST_EINT_GSE_1_NUM              11
+#define CUST_EINT_ACCDET_NUM              4
+#define CUST_EINT_ACCDET_DEBOUNCE_CN      0
+#define CUST_EINT_ACCDET_TYPE                            CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_ACCDET_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
+
+#define CUST_EINT_GSE_1_NUM              5
 #define CUST_EINT_GSE_1_DEBOUNCE_CN      0
-#define CUST_EINT_GSE_1_TYPE							CUST_EINTF_TRIGGER_HIGH
+#define CUST_EINT_GSE_1_TYPE                            CUST_EINTF_TRIGGER_FALLING
 #define CUST_EINT_GSE_1_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
+
+#define CUST_EINT_GSE_2_NUM              6
+#define CUST_EINT_GSE_2_DEBOUNCE_CN      0
+#define CUST_EINT_GSE_2_TYPE                            CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_GSE_2_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
+
+#define CUST_EINT_OTG_IDDIG_NUM              17
+#define CUST_EINT_OTG_IDDIG_DEBOUNCE_CN      0
+#define CUST_EINT_OTG_IDDIG_TYPE                            CUST_EINTF_TRIGGER_FALLING
+#define CUST_EINT_OTG_IDDIG_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_DISABLE
 
 #define CUST_EINT_MT6323_PMIC_NUM              25
 #define CUST_EINT_MT6323_PMIC_DEBOUNCE_CN      1
-#define CUST_EINT_MT6323_PMIC_TYPE							CUST_EINTF_TRIGGER_HIGH
+#define CUST_EINT_MT6323_PMIC_TYPE                            CUST_EINTF_TRIGGER_HIGH
 #define CUST_EINT_MT6323_PMIC_DEBOUNCE_EN      CUST_EINT_DEBOUNCE_ENABLE
 
 
