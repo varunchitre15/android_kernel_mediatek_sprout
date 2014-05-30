a1f5ffa1b187593f1e6616a96e22c1cc2fe685e1
Anmin Hsu <anmin.hsu@mediatek.com>
2014-05-30 19:02:08 +0800

    ARM: mt6582: sprout: Fix GPIO settings for charging indicator and volume keys
    Change-Id: I379ba6112b062ffa4eb897be527f723af9c80440
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

--- arch/arm/mach-mt6582/sprout/dct/dct/cust_kpd.h
+++ arch/arm/mach-mt6582/sprout/dct/dct/cust_kpd.h
@@ -16,19 +16,19 @@
 #define _CUST_KPD_H_
 #include <linux/input.h>
 #include <cust_eint.h>
-#define KPD_YES		1
-#define KPD_NO		0
+#define KPD_YES        1
+#define KPD_NO        0
 /* available keys (Linux keycodes) */
-#define KEY_CALL	KEY_SEND
-#define KEY_ENDCALL	KEY_END
+#define KEY_CALL    KEY_SEND
+#define KEY_ENDCALL    KEY_END
 #undef KEY_OK
-#define KEY_OK		KEY_REPLY	/* DPAD_CENTER */
-#define KEY_FOCUS	KEY_HP
-#define KEY_AT		KEY_EMAIL
-#define KEY_POUND	228	//KEY_KBDILLUMTOGGLE
-#define KEY_STAR	227	//KEY_SWITCHVIDEOMODE
-#define KEY_DEL 	KEY_BACKSPACE
-#define KEY_SYM		KEY_COMPOSE
+#define KEY_OK        KEY_REPLY    /* DPAD_CENTER */
+#define KEY_FOCUS    KEY_HP
+#define KEY_AT        KEY_EMAIL
+#define KEY_POUND    228    //KEY_KBDILLUMTOGGLE
+#define KEY_STAR    227    //KEY_SWITCHVIDEOMODE
+#define KEY_DEL     KEY_BACKSPACE
+#define KEY_SYM        KEY_COMPOSE
 /* KEY_HOME */
 /* KEY_BACK */
 /* KEY_VOLUMEDOWN */
@@ -45,7 +45,7 @@
 /* KEY_ENTER */
 /* KEY_LEFTSHIFT */
 /* KEY_COMMA */
-/* KEY_DOT */		/* PERIOD */
+/* KEY_DOT */        /* PERIOD */
 /* KEY_SLASH */
 /* KEY_LEFTALT */
 /* KEY_RIGHTALT */
@@ -66,10 +66,10 @@
 #define MTK_PMIC_PWR_KEY    8
 
 /* HW keycode [0 ~ 71] -> Linux keycode */
-#define KPD_INIT_KEYMAP()	\
-{	\
-	[0] = KEY_VOLUMEDOWN,		\
-}	 
+#define KPD_INIT_KEYMAP()    \
+{    \
+    [0] = KEY_VOLUMEUP,        \
+}
 /*****************************************************************/
 /*******************Preload Customation***************************/
 /*****************************************************************/
@@ -77,25 +77,25 @@
 
 #define KPD_PWRKEY_GPIO_DIN  0
 
-#define  KPD_DL_KEY2  0    /* KEY_VOLUMEDOWN */
+#define  KPD_DL_KEY1  0    /* KEY_VOLUMEUP */
 #define  KPD_DL_KEY3  8    /* KEY_POWER */
 /*****************************************************************/
 /*******************Uboot Customation***************************/
 /*****************************************************************/
-#define MT65XX_FACTORY_KEY  0    /* KEY_VOLUMEDOWN */
+#define MT65XX_RECOVERY_KEY  0    /* KEY_VOLUMEUP */
 /*****************************************************************/
 /*******************factory Customation***************************/
 /*****************************************************************/
-#define KEYS_PWRKEY_MAP		{ KEY_POWER, "Power" }
-#define DEFINE_KEYS_KEYMAP(x)		\
- struct key x[] = {     	\
- 	KEYS_PWRKEY_MAP,		\
-  { KEY_VOLUMEDOWN,   "VLDown"  }, \
+#define KEYS_PWRKEY_MAP        { KEY_POWER, "Power" }
+#define DEFINE_KEYS_KEYMAP(x)        \
+ struct key x[] = {         \
+     KEYS_PWRKEY_MAP,        \
+  { KEY_VOLUMEUP,   "VLUp"  }, \
 }
-#define CUST_KEY_UP  KEY_VOLUMEUP   
-#define CUST_KEY_VOLUP  KEY_VOLUMEUP   
-#define CUST_KEY_DOWN  KEY_VOLUMEDOWN   
-#define CUST_KEY_VOLDOWN  KEY_VOLUMEDOWN   
+#define CUST_KEY_UP  KEY_VOLUMEUP
+#define CUST_KEY_VOLUP  KEY_VOLUMEUP
+#define CUST_KEY_DOWN  KEY_VOLUMEDOWN
+#define CUST_KEY_VOLDOWN  KEY_VOLUMEDOWN
 /*****************************************************************/
 /*******************recovery Customation****************************/
 /*****************************************************************/
