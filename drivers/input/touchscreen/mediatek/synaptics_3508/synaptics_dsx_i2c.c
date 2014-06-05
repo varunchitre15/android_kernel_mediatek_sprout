5550450d5d04ac982e3e7a1d28d2541f52e8a9e6
Anmin Hsu <anmin.hsu@mediatek.com>
2014-06-05 15:23:30 +0800

    input: touchscreen: mediatek: s3508: update S3508 touch driver for CTS fail issue
    Change-Id: Ic5c4e1f4c09e66419126c1d8fdb11f42e7340305
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

--- drivers/input/touchscreen/mediatek/synaptics_3508/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/mediatek/synaptics_3508/synaptics_dsx_i2c.c
@@ -1,6 +1,7 @@
 /*
-*  Copyright (C) 2014 MediaTek Inc. 
-*  Based on code by
+* Copyright (C) 2014 MediaTek Inc.
+* Modification based on code covered by the below mentioned copyright
+* and/or permission notice(s).
 */
 /*
  * Synaptics DSX touchscreen driver
@@ -399,11 +400,11 @@ static struct synaptics_rmi4_exp_fn_data exp_data;
 
 static struct device_attribute attrs[] = {
 #ifdef CONFIG_HAS_EARLYSUSPEND
-	__ATTR(full_pm_cycle, (S_IRUGO | S_IWUGO),
+	__ATTR(full_pm_cycle, 0660,
 			synaptics_rmi4_full_pm_cycle_show,
 			synaptics_rmi4_full_pm_cycle_store),
 #endif
-	__ATTR(reset, S_IWUGO,
+	__ATTR(reset, 0660,
 			synaptics_rmi4_show_error,
 			synaptics_rmi4_f01_reset_store),
 	__ATTR(productinfo, S_IRUGO,
@@ -415,10 +416,10 @@ static struct device_attribute attrs[] = {
 	__ATTR(flashprog, S_IRUGO,
 			synaptics_rmi4_f01_flashprog_show,
 			synaptics_rmi4_store_error),
-	__ATTR(0dbutton, (S_IRUGO | S_IWUGO),
+	__ATTR(0dbutton, 0660,
 			synaptics_rmi4_0dbutton_show,
 			synaptics_rmi4_0dbutton_store),
-	__ATTR(suspend, S_IWUGO,
+	__ATTR(suspend, 0660,
 			synaptics_rmi4_show_error,
 			synaptics_rmi4_suspend_store),
 };
--- drivers/input/touchscreen/mediatek/synaptics_3508/synaptics_dsx_rmi_dev.c
+++ drivers/input/touchscreen/mediatek/synaptics_3508/synaptics_dsx_rmi_dev.c
@@ -1,6 +1,7 @@
 /*
-*  Copyright (C) 2014 MediaTek Inc. 
-*  Based on code by
+* Copyright (C) 2014 MediaTek Inc.
+* Modification based on code covered by the below mentioned copyright
+* and/or permission notice(s).
 */
 /*
  * Synaptics DSX touchscreen driver
@@ -79,7 +80,7 @@ struct rmidev_data {
 static struct bin_attribute attr_data = {
 	.attr = {
 		.name = "data",
-		.mode = (S_IRUGO | S_IWUGO),
+		.mode = 0660,
 	},
 	.size = 0,
 	.read = rmidev_sysfs_data_show,
@@ -87,10 +88,10 @@ static struct bin_attribute attr_data = {
 };
 
 static struct device_attribute attrs[] = {
-	__ATTR(open, S_IWUGO,
+	__ATTR(open, 0660,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_open_store),
-	__ATTR(release, S_IWUGO,
+	__ATTR(release, 0660,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_release_store),
 	__ATTR(attn_state, S_IRUGO,
