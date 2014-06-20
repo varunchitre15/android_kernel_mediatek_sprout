ee1b9bc8b3e584144aab61facba83b2fd9e47af3
Anmin Hsu <anmin.hsu@mediatek.com>
2014-06-20 18:49:58 +0800

    mediatek: touchscreen: S3203: Update Touch driver firmware Download policy & Newest Firmware for 100Hz report rate
    Change-Id: I13d0a768928ec0f77175d99af3c578af2587c573
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>

--- arch/arm/mach-mt6582/sprout/touchpanel/S3203/tpd_custom_synaptics_rmi4.h
+++ arch/arm/mach-mt6582/sprout/touchpanel/S3203/tpd_custom_synaptics_rmi4.h
@@ -1,3 +1,17 @@
+/*
+* Copyright (C) 2011-2014 MediaTek Inc.
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef TOUCHPANEL_H__
 #define TOUCHPANEL_H__
 
@@ -11,7 +25,7 @@
 #define CUSTOM_MAX_WIDTH (480)
 #define CUSTOM_MAX_HEIGHT (854)
 
-//#define TPD_UPDATE_FIRMWARE
+#define TPD_UPDATE_FIRMWARE
 
 //#define TPD_HAVE_BUTTON
 #define TPD_BUTTON_HEIGH        (100)
@@ -22,6 +36,12 @@
                             {400,854,100,TPD_BUTTON_HEIGH}}
 
 #define TPD_POWER_SOURCE_CUSTOM         MT6323_POWER_LDO_VGP1//MT65XX_POWER_LDO_VGP1
+#define LCD_X           480
+#define LCD_Y           854
+
+#define TPD_UPDATE_FIRMWARE
+//#define HAVE_TOUCH_KEY
+
 
 //#define TPD_HAVE_CALIBRATION
 //#define TPD_CALIBRATION_MATRIX  {2680,0,0,0,2760,0,0,0};
