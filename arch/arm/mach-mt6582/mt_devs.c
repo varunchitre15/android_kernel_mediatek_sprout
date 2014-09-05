49e9119804a16c0c2d024123529cead87e0ed4cf
Cylen Yao <cylen.yao@mediatek.com>
2014-09-05 12:15:06 +0800

    ARM: mt6582: lens: OEM Partition for AF driver
    Note : depends on c/180623
    
    Change-Id: Ia46e87e2e089597c4a3b5d9fd32c9964d0e7cc9a
    Signed-off-by: Cylen Yao <cylen.yao@mediatek.com>

--- arch/arm/mach-mt6582/mt_devs.c
+++ arch/arm/mach-mt6582/mt_devs.c
@@ -1819,6 +1819,15 @@ static struct platform_device actuator_dev = {
 	.name		  = "lens_actuator",
 	.id		  = -1,
 };
+static struct platform_device actuator_dev2 = {
+	.name		  = "lens_actuator2",
+	.id		  = -1,
+};
+static struct platform_device actuator_dev3 = {
+	.name		  = "lens_actuator3",
+	.id		  = -1,
+};
+
 /*=======================================================================*/
 /* MT6575 jogball                                                        */
 /*=======================================================================*/
@@ -2445,6 +2454,18 @@ retval = platform_device_register(&dummychar_device);
 //=======================================================================
 #if 1  //defined(CONFIG_ACTUATOR)
     retval = platform_device_register(&actuator_dev);
+    if (retval != 0){
+        return retval;
+    }
+#endif
+#if 1  //defined(CONFIG_ACTUATOR)
+    retval = platform_device_register(&actuator_dev2);
+    if (retval != 0){
+        return retval;
+    }
+#endif
+#if 1  //defined(CONFIG_ACTUATOR)
+    retval = platform_device_register(&actuator_dev3);
     if (retval != 0){
         return retval;
     }
