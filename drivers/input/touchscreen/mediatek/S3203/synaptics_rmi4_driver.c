12734924488c8ba6b1174c10ed77e009c5c71166
Cylen Yao <cylen.yao@mediatek.com>
2014-10-27 17:36:43 +0800

    mediatek: Touch: Firmware upgrade from SD card
    1. Fix the problem of CTP firmware upgrade from SD card is not working.
    2. gt9xx_drivers.c GPL declaration
    
    Change-Id: Ib081ca300983b87a187f39e54452af05b1036f50
    Signed-off-by: Cylen Yao <cylen.yao@mediatek.com>

--- drivers/input/touchscreen/mediatek/S3203/synaptics_rmi4_driver.c
+++ drivers/input/touchscreen/mediatek/S3203/synaptics_rmi4_driver.c
@@ -1798,7 +1798,6 @@ static int tpd_firmware_updater(void *unused)
 {
     int retval = 0;
 
-    synaptics_rmi4_detection_work(NULL);
     synaptics_fw_updater_s3203(synaImage);
 
     retval = tpd_rmi4_read_pdt(ts);
@@ -1899,15 +1898,8 @@ static int tpd_probe(struct i2c_client *client, const struct i2c_device_id *id)
     input_set_abs_params(tpd->dev, ABS_MT_TOOL_TYPE,
                 0, MT_TOOL_MAX, 0, 0);
 #endif
+    synaptics_rmi4_detection_work(NULL);
 
-#if 0
-    firmware_thread = kthread_run(tpd_firmware_updater, 0, "firmware_updater");
-    if (IS_ERR(firmware_thread))
-    {
-        retval = PTR_ERR(firmware_thread);
-        TPD_DMESG(TPD_DEVICE " failed to create kernel thread: %d\n", retval);
-    }
-#endif
 
     tpd_i2c_read_data(client, ts->f11.ctrl_base+6, &TP_Max_X,2);
     tpd_i2c_read_data(client, ts->f11.ctrl_base+8,&TP_Max_Y,2);
