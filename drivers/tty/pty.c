bbe8a0918800888ef7a66ebb46e049767f2cc658
Cylen Yao <cylen.yao@mediatek.com>
2015-01-05 15:59:28 +0800

    tty: remove mediatek modification
    Problem: CTS fail: android.bionic.stdlib#ptsname_r_ENOTTY@armeabi-v7a
    Bug num: 18787840
    Details:
        There are several modifications in pty.c made by mediatek, and it's
        unnecessary for Sprout.
    
    Change-Id: Ief654c2ec49890e05961ec763d5820a67143318e
    Signed-off-by: Cylen Yao <cylen.yao@mediatek.com>

--- drivers/tty/pty.c
+++ drivers/tty/pty.c
@@ -24,7 +24,7 @@
 #include <linux/devpts_fs.h>
 #include <linux/slab.h>
 #include <linux/mutex.h>
-//#include <asm/system.h>
+
 
 #ifdef CONFIG_UNIX98_PTYS
 static struct tty_driver *ptm_driver;
@@ -47,13 +47,9 @@ static void pty_close(struct tty_struct *tty, struct file *filp)
 	wake_up_interruptible(&tty->read_wait);
 	wake_up_interruptible(&tty->write_wait);
 	tty->packet = 0;
-	tty->peer_stops = 0;
-
 	/* Review - krefs on tty_link ?? */
 	if (!tty->link)
 		return;
-	tty->link->peer_stops = 0;
-
 	set_bit(TTY_OTHER_CLOSED, &tty->link->flags);
 	wake_up_interruptible(&tty->link->read_wait);
 	wake_up_interruptible(&tty->link->write_wait);
@@ -118,7 +114,7 @@ static int pty_write(struct tty_struct *tty, const unsigned char *buf, int c)
 {
 	struct tty_struct *to = tty->link;
 
-	if (tty->stopped || tty->peer_stops)
+	if (tty->stopped)
 		return 0;
 
 	if (c > 0) {
@@ -141,7 +137,7 @@ static int pty_write(struct tty_struct *tty, const unsigned char *buf, int c)
 
 static int pty_write_room(struct tty_struct *tty)
 {
-	if (tty->stopped || tty->peer_stops)
+	if (tty->stopped)
 		return 0;
 	return pty_space(tty->link);
 }
@@ -316,22 +312,6 @@ done:
 	return 0;
 }
 
-/* Unix98 devices */
-#ifdef CONFIG_UNIX98_PTYS
-static int tty_count;
-static int pty_count;
-
-static inline void pty_inc_count(void)
-{
-		pty_count = (++tty_count) / 2;
-}
-
-static inline void pty_dec_count(void)
-{
-		pty_count = (--tty_count) / 2;
-}
-#endif
-
 /**
  *	pty_common_install		-	set up the pty pair
  *	@driver: the pty driver
@@ -401,12 +381,6 @@ static int pty_common_install(struct tty_driver *driver, struct tty_struct *tty,
 
 	tty_driver_kref_get(driver);
 	tty->count++;
-	if(!legacy){
-#ifdef CONFIG_UNIX98_PTYS    	
-		pty_inc_count(); /* tty */
-		pty_inc_count(); /* tty->link */
-#endif        
-	}
 	return 0;
 err_free_termios:
 	if (legacy)
@@ -522,7 +496,6 @@ static void __init legacy_pty_init(void)
 	if (IS_ERR(pty_slave_driver))
 		panic("Couldn't allocate pty slave driver");
 
-	pty_driver->owner = THIS_MODULE;
 	pty_driver->driver_name = "pty_master";
 	pty_driver->name = "pty";
 	pty_driver->major = PTY_MASTER_MAJOR;
@@ -539,7 +512,6 @@ static void __init legacy_pty_init(void)
 	pty_driver->other = pty_slave_driver;
 	tty_set_operations(pty_driver, &master_pty_ops_bsd);
 
-	pty_slave_driver->owner = THIS_MODULE;
 	pty_slave_driver->driver_name = "pty_slave";
 	pty_slave_driver->name = "ttyp";
 	pty_slave_driver->major = PTY_SLAVE_MAJOR;
@@ -567,11 +539,6 @@ static inline void legacy_pty_init(void) { }
 
 static struct cdev ptmx_cdev;
 
-#define TCOOFF 0
-#define TCOON 1
-#define TCIOFF 2
-#define TCION 3
-
 static int pty_unix98_ioctl(struct tty_struct *tty,
 			    unsigned int cmd, unsigned long arg)
 {
@@ -588,19 +555,6 @@ static int pty_unix98_ioctl(struct tty_struct *tty,
 		return put_user(tty->index, (unsigned int __user *)arg);
 	case TIOCSIG:    /* Send signal to other side of pty */
 		return pty_signal(tty, (int) arg);
-	case TCXONC: /* Flow Control */
-		switch (arg) {
-			case TCIOFF:
-				break;
-			case TCION:
-				tty->link->peer_stops=0;           
-				if (waitqueue_active(&tty->link->write_wait))
-					wake_up_interruptible(&tty->link->write_wait);
-				break;
-			default:
-				return -EINVAL;
-		}
-		return 0;
 	}
 
 	return -ENOIOCTLCMD;
@@ -618,16 +572,8 @@ static int pty_unix98_ioctl(struct tty_struct *tty,
 static struct tty_struct *ptm_unix98_lookup(struct tty_driver *driver,
 		struct inode *ptm_inode, int idx)
 {
-	////struct tty_struct *tty = devpts_get_tty(ptm_inode, idx);
-	struct tty_struct *tty;
-
-	mutex_lock(&devpts_mutex);
-	tty = devpts_get_priv(ptm_inode);
-	mutex_unlock(&devpts_mutex);
-
-	if (tty)
-		tty = tty->link;
-	return tty;
+	/* Master must be open via /dev/ptmx */
+	return ERR_PTR(-EIO);
 }
 
 /**
@@ -663,7 +609,6 @@ static int pty_unix98_install(struct tty_driver *driver, struct tty_struct *tty)
 
 static void pty_unix98_remove(struct tty_driver *driver, struct tty_struct *tty)
 {
-	pty_dec_count();
 }
 
 /* this is called once with whichever end is closed last */
@@ -703,7 +648,6 @@ static const struct tty_operations pty_unix98_ops = {
 	.unthrottle = pty_unthrottle,
 	.set_termios = pty_set_termios,
 	.shutdown = pty_unix98_shutdown,
-	.ioctl = pty_unix98_ioctl,
 	.cleanup = pty_cleanup,
 };
 
@@ -811,7 +755,6 @@ static void __init unix98_pty_init(void)
 	if (IS_ERR(pts_driver))
 		panic("Couldn't allocate Unix98 pts driver");
 
-	ptm_driver->owner = THIS_MODULE;
 	ptm_driver->driver_name = "pty_master";
 	ptm_driver->name = "ptm";
 	ptm_driver->major = UNIX98_PTY_MASTER_MAJOR;
@@ -828,7 +771,6 @@ static void __init unix98_pty_init(void)
 	ptm_driver->other = pts_driver;
 	tty_set_operations(ptm_driver, &ptm_unix98_ops);
 
-	pts_driver->owner = THIS_MODULE;
 	pts_driver->driver_name = "pty_slave";
 	pts_driver->name = "pts";
 	pts_driver->major = UNIX98_PTY_SLAVE_MAJOR;
