300d36c55ce0ad687152575faf9b135c1c08243e
yang-cy.chen <yang-cy.chen@mediatek.com>
2015-07-01 15:27:07 +0800

    mediatek: fix memory leak of slabtrace tool
    Problem:
    Use single_open as the .open function in file_operations
    and seq_release as the .release function will cause a memory leak.
    
    Solution:
    using single_release as the .release function.
    
    Bug num: 22213471
    
    Change-Id: I79c6a77fc74e6c4117751b5b033a2c60b48e5283
    Signed-off-by: yang-cy.chen <yang-cy.chen@mediatek.com>

--- drivers/misc/mediatek/kernel/mtk_memcfg.c
+++ drivers/misc/mediatek/kernel/mtk_memcfg.c
@@ -204,7 +204,7 @@ static const struct file_operations proc_slabtrace_operations = {
 	.open = slabtrace_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
-	.release = seq_release,
+	.release = single_release,
 };
 #endif
 
